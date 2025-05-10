15e6a9dc59e081a971ea77226f657cfe0c251ef5
SvetlanaFrolovaSvekla <frolova.savelya-486@yandex.com>
2025-05-10 21:02:37 +0600

    44

--- server/Index.js
+++ server/Index.js
@@ -20,18 +20,7 @@ const allowedOrigins = [
 
 // Настройка CORS - более простая версия
 app.use(cors({
-    origin: function (origin, callback) {
-        // Разрешить запросы без origin (например, мобильные приложения)
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            console.log(`CORS block for origin: ${origin}`);
-            // В рабочем режиме лучше просто отклонять запросы без ошибки
-            callback(null, true); // Временно разрешаем все origins для отладки
-        }
-    },
+    origin: '*', // временно разрешить все источники
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
@@ -74,26 +63,7 @@ app.get('/test-cors', (req, res) => {
 app.use(errorHandler)
 
 
-app.use((req, res, next) => {
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "http://localhost:3000"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
 
-    next();
-});
 
 // await sequelize.sync() - функция сравнивает бд со схемой данных
 // await sequelize.authenticate(); // Подключение к базе данных
