b519fe47b96497a76af871ee9ddd2d3905dc1b43
SvetlanaFrolovaSvekla <frolova.savelya-486@yandex.com>
2025-05-10 20:04:02 +0600

    изменение в cors

--- server/Index.js
+++ server/Index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.disable('x-powered-by');
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true // если в будущем будешь использовать cookies или авторизацию с сессиями
+}))
+
 
 app.use(express.json())
 
