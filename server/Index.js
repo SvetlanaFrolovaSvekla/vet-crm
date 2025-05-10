fd7a7376caa7bd637f436017116504de978c5209
SvetlanaFrolovaSvekla <frolova.savelya-486@yandex.com>
2025-05-10 20:51:48 +0600

    cros 3

--- server/Index.js
+++ server/Index.js
@@ -72,6 +72,29 @@ app.get('/test-cors', (req, res) => {
 });
 
 app.use(errorHandler)
+
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "http://localhost:3000"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
+
 // await sequelize.sync() - функция сравнивает бд со схемой данных
 // await sequelize.authenticate(); // Подключение к базе данных
 const start = async () => {
