396223077d1b9dd47b86338c3ed9ebd14006029c
SvetlanaFrolovaSvekla <frolova.savelya-486@yandex.com>
2025-05-10 22:27:14 +0600

    Cors

--- server/Index.js
+++ server/Index.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.disable('x-powered-by');
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 app.use(express.json())
 
